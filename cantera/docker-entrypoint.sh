06a703b13277cdc7d0fd96525beffd3ac443da33
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-03-24 14:01:01 -0400

    Update for WSSCI

--- cantera/docker-entrypoint.sh
+++ cantera/docker-entrypoint.sh
@@ -9,12 +9,12 @@ echo "$1"
 
 # If the run command is the default, do some initialization first
 if [ "$(which "$1")" = "/usr/local/bin/start-singleuser.sh" ]; then
-  # Clone sample notebooks to user's notebook directory.  Assume $NB_USER's work
-  # directory if notebook directory not explicitly set.  `git clone` will fail
+  # Clone sample notebooks to user's notebook directory. Assume $NB_USER's work
+  # directory if notebook directory not explicitly set. `git clone` will fail
   # if target directory already exists and is not empty, which likely means
   # that we've already done it, so just ignore.
   : ${NOTEBOOK_DIR:=/home/$NB_USER/work}
-  git clone https://github.com/Cantera/essci-2018-materials \
+  git clone https://github.com/Cantera/wssci-2018-materials \
     "$NOTEBOOK_DIR/workshop_files" || cd "$NOTEBOOK_DIR/workshop_files" && git pull || true
 fi
 
